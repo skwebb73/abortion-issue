30f1bfe0d445094f08dadfc3cca7ecdc33d24ee3
Susie Webb <SusieKWebb@MacBook-Pro-168.lan>
2022-10-10 01:01:21 -0400

    quiz updated

--- js/quiz.js
+++ js/quiz.js
@@ -1,50 +1,65 @@
-$('#quiz').quiz({
-  //resultsScreen: '#results-screen',
-  //counter: false,
-  //homeButton: '#custom-home',
-  counterFormat: 'Question %current of %total',
-  questions: [
-    {
-      'q': 'Is jQuery required for this plugin?',
+  $('#quiz').quiz({
+    //resultsScreen: '#results-screen',
+    //counter: false,
+    //homeButton: '#custom-home',
+    counterFormat: 'Question %current of %total',
+    questions: [
+      {
+        'q': 'When was abortion first banned in North Carolina',
+        'options': [
+          '1973',
+          '1881',
+          '2019',
+          '1870'
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Correct!',
+        'incorrectResponse': 'Incorrect'
+      },
+      {
+        'q': "Who created the content for The Daily Tar Heel's Abortion Special Issue?",
+        'options': [
+          'Only newsroom editors',
+          'Female and nonbinary members of the newsroom'
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Correct!',
+        'incorrectResponse': 'Incorrect'
+      },
+      {
+        'q': 'When was Roe v. Wade overturned?',
       'options': [
-        'Yes',
-        'No'
+        'March 2022',
+        'July 2022',
+        'August 2022',
+        'June 2022'
       ],
-      'correctIndex': 0,
-      'correctResponse': 'Good job, that was obvious.',
-      'incorrectResponse': 'Well, if you don\'t include it, your quiz won\'t work'
-    },
-    {
-      'q': 'How do you use it?',
-      'options': [
-        'Include jQuery, that\'s it!',
-        'Include jQuery and the plugin javascript.',
-        'Include jQuery, the plugin javascript, the optional plugin css, required markup, and the javascript configuration.'
-      ],
-      'correctIndex': 2,
-      'correctResponse': 'Correct! Sounds more complicated than it really is.',
-      'incorrectResponse': 'Come on, it\'s not that easy!'
-    },
-    {
-      'q': 'The plugin can be configured to require a perfect score.',
-      'options': [
-        'True',
-        'False'
-      ],
-      'correctIndex': 0,
-      'correctResponse': 'You\'re a genius! You just set allowIncorrect to true.',
-      'incorrectResponse': 'Why you have no faith!? Just set allowIncorrect to true.'
-    },
-    {
-      'q': 'How do you specify the questions and answers?',
-      'options': [
-        'MySQL database',
-        'In the HTML',
-        'In the javascript configuration'
-      ],
-      'correctIndex': 2,
-      'correctResponse': 'Correct! Refer to the documentation for the structure.',
-      'incorrectResponse': 'Wrong! Do it in the javascript configuration. You might need to read the documentation.'
-    }
-  ]
-});
+      'correctIndex': 3,
+      'correctResponse': 'Correct!',
+      'incorrectResponse': 'Incorrect'
+      },
+      {
+        'q': 'How many abortion clinics are there in North Carolina?',
+        'options': [
+          '14',
+          '20',
+          '8'
+        ],
+        'correctIndex': 0,
+        'correctResponse': 'Correct!',
+        'incorrectResponse': 'Incorrect'
+      },
+      {
+        'q': 'Which act was passed by the N.C. General Assembly in 2011',
+        'options': [
+          'Abortion Rights Act',
+          "Woman's Right to Abortion Act",
+          "Woman's Right to Know Act",
+          'Abortion Restrictions Act'
+        ],
+        'correctIndex': 2,
+        'correctResponse': 'Correct!',
+        'incorrectResponse': 'Incorrect'
+      }
+    ]
+  });
